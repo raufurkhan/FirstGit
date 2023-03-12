fd3dee550df8dbe55c8b53488aef20daf19f573d
raufurkhan <raufurkhan82@gmail.com>
2023-03-12 11:52:14 +0530

    DOM assignment

---
+++ DOM/dom.js
@@ -0,0 +1,19 @@
+console.dir(document);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+document.title="check it";
+document.all[10].textContent='hello'
+
+//let headerTitle=document.getElementById("header-title");
+//headerTitle.style.borderBottom='solid 3px #000';
+let header=document.getElementById("main-header");
+console.log(header);
+header.style.borderBottom='solid 3px #000';
+let title=document.getElementsByClassName("title");
+console.log(title[0]);
+title[0].style.fontWeight='bold';
+title[0].style.color='green';
