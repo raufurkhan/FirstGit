26d6cae22702127702cce0ea42d3daf061811dd5
raufurkhan <raufurkhan82@gmail.com>
2023-03-13 09:48:36 +0530

    queryselector

--- DOM/dom.js
+++ DOM/dom.js
@@ -34,3 +34,20 @@ for(let i=0;i<li.length;i++){
     li[i].style.fontWeight='bold'
 
 }
+
+
+let secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green'
+let thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility='hidden'
+
+let items=document.querySelectorAll('.list-group-item');
+items[1].style.color='green'
+
+let odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
+let even=document.querySelectorAll('.list-group-item:nth-child(even)');
+
+for(let i=0;i<odd.length;i++){
+
+    odd[i].style.backgroundColor='green'
+}
