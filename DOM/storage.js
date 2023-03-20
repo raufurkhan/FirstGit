e1ba0df6a730498083f90fd1f3b0d79a6f207fee
raufurkhan <raufurkhan82@gmail.com>
2023-03-20 22:33:32 +0530

    Scale the app to more users

--- DOM/storage.js
+++ DOM/storage.js
@@ -21,12 +21,21 @@ function onSubmit(e){
    
        }
        obj_serialize=JSON.stringify(obj);
-       localStorage.setItem('myobj',obj_serialize);
+       localStorage.setItem(email.value,obj_serialize);
        console.log(obj_serialize);
    
        
    
        let obj_deserialize=JSON.parse(localStorage.getItem('myobj'));
        console.log(obj_deserialize)
-   
+       let userList=document.getElementById('listofusers');
+
+       let li=document.createElement('li');
+           // textNode_username= document.createTextNode(username.value);
+           // textNode_email= document.createTextNode(email.value);
+            li.appendChild(document.createTextNode(username.value))
+            li.appendChild(document.createTextNode("----"))
+            li.appendChild(document.createTextNode(email.value))
+
+            listofusers.appendChild(li);
 }
