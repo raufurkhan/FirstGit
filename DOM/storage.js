a242346846428c302d3ba60b428cc918dd558268
raufurkhan <raufurkhan82@gmail.com>
2023-03-20 21:44:32 +0530

    store in the right format

--- DOM/storage.js
+++ DOM/storage.js
@@ -12,7 +12,21 @@ function onSubmit(e){
     e.preventDefault();
     console.log(username.value);
     
-    localStorage.setItem('name',username.value);
-    localStorage.setItem('email',email.value);
+   // localStorage.setItem('name',username.value);
+    //localStorage.setItem('email',email.value);
+
+    let obj={
+        name: username.value,
+        email: email.value
+   
+       }
+       obj_serialize=JSON.stringify(obj);
+       localStorage.setItem('myobj',obj_serialize);
+       console.log(obj_serialize);
+   
+       
+   
+       let obj_deserialize=JSON.parse(localStorage.getItem('myobj'));
+       console.log(obj_deserialize)
    
 }
