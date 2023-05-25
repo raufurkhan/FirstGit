f1abdfe86ac6dbd931e669ddd05cf8a095beba90
raufurkhan <127369771+raufurkhan@users.noreply.github.com>
2023-05-25 09:38:50 +0530

    lets start coding

---
+++ ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem(){
+
+return (<div>
+<div><h2>
+ExpenseItem !</h2>
+</div>
+
+<div>
+<h4>Food Rs 10</h4>
+
+<h4>Petrol Rs 100</h4>
+
+<h4>Movies Rs 200</h4>
+</div>
+
+</div>)
+}
+
+
+export default ExpenseItem
