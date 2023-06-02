7d694ea810ea7a8847cd621deaa1f5747eb63767
raufurkhan <raufurkhan82@gmail.com>
2023-06-02 21:33:54 +0530

    retoring old newexpense file

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
-  return (
+const NewExpense = (props) => {
+  
+    const saveExpenseDataHandler=(enteredExpenseData)=>{
+
+        const expenseData={
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        props.onAddExpense(expenseData)
+    }
+  
+    return (
     <div className="new-expense">
-    <ExpenseForm/> 
+    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/> 
     </div>
-  );
-};
+  )
+}
 
 export default NewExpense;
