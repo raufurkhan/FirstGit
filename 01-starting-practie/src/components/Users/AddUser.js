e94649abdf774155609094c8c108651582f1d289
raufurkhan <raufurkhan82@gmail.com>
2023-06-24 14:00:39 +0530

    Ref and controlled

--- 01-starting-practie/src/components/Users/AddUser.js
+++ 01-starting-practie/src/components/Users/AddUser.js
@@ -1,61 +1,66 @@
-import React ,{useState}from "react"
-import Card from '../UI/Card'
-import classes from './AddUser.module.css'
-import Button from "../UI/Button"
-import ErrorModal from "../UI/ErrorModal"
-
-const AddUser=props=>{
-
-    const[enteredUsername,setUsername]=useState('')
-    const[enteredAge,setAge]=useState('')
-   const[error,setError] =useState()
+import React, { useState, useRef } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 
 
-    const addSubmitHandler=(event)=>{
-        event.preventDefault();
-        if(enteredUsername.trim().length===0 || enteredAge.trim().length===0){
-        setError({title:'invalid input',
-    message:'enter correct name'})
-        return;}
-        if(+enteredAge<1){
-        setError({title:'invalid age',
-    message:'enter correct age'})
-        return}
-        props.onAddUser(enteredUsername,enteredAge)
-        setUsername('')
-        setAge('')
+const AddUser = (props) => {
+  const nameInputref = useRef();
+  const ageRef = useRef();
+
+  const [error, setError] = useState();
+
+  const addSubmitHandler = (event) => {
+    const enteredNameRef=nameInputref.current.value
+    const enteredAgeRef=ageRef.current.value
+    event.preventDefault();
+    if (enteredNameRef.trim().length === 0 || enteredAgeRef.trim().length === 0) {
+      setError({ title: "invalid input", message: "enter correct name" });
+      return;
     }
+    if (+enteredAgeRef < 1) {
+      setError({ title: "invalid age", message: "enter correct age" });
+      return;
+    }
+    props.onAddUser(enteredNameRef, enteredAgeRef);
+    nameInputref.current.value=''
+    ageRef.current.value=''
+  };
 
-    const addAgeHandler=(e)=>{
-      setAge(e.target.value)
-    }
 
-    const addUserHandler=e=>{
-        setUsername(e.target.value)
-    }
-    const errorHandler=()=>{
-        setError(null)
-    }
-return (
+  const errorHandler = () => {
+    setError(null);
+  };
+  return (
     <div>
-    {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
-<Card className={classes.input}>
-<form onSubmit={addSubmitHandler}>
-<label htmlFor='username'>Name</label>
-<input type='text' id='username'onChange={addUserHandler} value={enteredUsername}></input>
-<label htmlFor='age'>Age</label>
-<input type='number' id='age' onChange={addAgeHandler} value={enteredAge}></input>
-<Button type='submit'> Add User</Button>
-</form>
-</Card>
-</div>
-
-
-
-)
-}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addSubmitHandler}>
+          <label htmlFor="username">Name</label>
+          <input
+            type="text"
+            id="username"
+            ref={nameInputref}
+          ></input>
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            ref={ageRef}
+          ></input>
+          <Button type="submit"> Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
 
 export default AddUser;
-
-
