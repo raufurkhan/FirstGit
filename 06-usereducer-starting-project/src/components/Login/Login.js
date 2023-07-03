096e69caf3c50f9d242562b3dc381a9f1fd7088a
raufurkhan <raufurkhan82@gmail.com>
2023-07-03 18:34:45 +0530

    Rules of Hooks

--- 06-usereducer-starting-project/src/components/Login/Login.js
+++ 06-usereducer-starting-project/src/components/Login/Login.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import AuthContext from '../../store/auth-context';
+import Input from '../UI/Input/Input';
 
 
 const emailReducer=(state,action)=>{
@@ -109,34 +110,16 @@ const ct=useContext(AuthContext)
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+       <Input id="email" label='E-mail' type='email' onChange={emailChangeHandler}
+       onBlur={validateEmailHandler}
+       isValid={emailValid}
+       value={emailState.value}
+       ></Input>
+        <Input id="password" label='Passord' type='password' onChange={passwordChangeHandler}
+       onBlur={validatePasswordHandler}
+       isValid={passwordValid}
+       value={passwordState.value}
+       ></Input>
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
